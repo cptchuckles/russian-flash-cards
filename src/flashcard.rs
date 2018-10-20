52a945d6f070d85d725c66de44f64245d897205d
cptchuckles <image1996@gmail.com>
2018-10-19 22:01:57 -0500

    More idiomatic Rust

--- src/flashcard.rs
+++ src/flashcard.rs
@@ -17,7 +17,7 @@ impl Deck {
                vars: HashMap::new(),
         }
     }
-    
+
     pub fn new_from_file(file: &str) -> Option<Deck> {
         let d = match load_deck_from_file(file) {
             Ok(deck) => Some(deck),
@@ -28,23 +28,23 @@ impl Deck {
         };
         d
     }
-    
+
     pub fn clear(&mut self) {
         self.cols = 0;
         self.content.clear();
         self.vars.clear();
     }
-    
+
     pub fn get_card(&self, ind: usize) -> Option<Vec<String>> {
         if ind >= self.content.len() {return None;}
         let s:Vec<&str> = self.content[ind].split(':').collect();
-        let mut r: Vec<String> = Vec::with_capacity(s.len());
-        for ss in s {
-            r.push(String::from(ss));
-        }
-        Some(r)
+
+        let thing = s.iter()
+                     .map(|ss| ss.to_string())
+                     .collect::<Vec<String>>();
+        Some(thing)
     }
-    
+
     pub fn get_card_data(&self, ind: usize) -> Option<String> {
         if ind >= self.content.len() {return None;}
         let r = &self.content[ind];
@@ -55,9 +55,9 @@ impl Deck {
 pub fn load_deck_from_file(file: &str) -> Result<Deck, String> {
     let f = File::open(file);
     if f.is_err() { return Err(format!("Bad filename: {}", file)); }
-    
+
     let buf = BufReader::new(f.unwrap());
-    
+
     let mut deck = Deck::new();
     for line in buf.lines().map(|l| l.unwrap()) {
         let token:char = line.chars().next().unwrap();
@@ -67,8 +67,7 @@ pub fn load_deck_from_file(file: &str) -> Result<Deck, String> {
                 if let Some(space) = line.find(' ')
                 {
                     let (k, v) = line.split_at(space);
-                    let v = String::from(v.trim());
-                    deck.vars.insert(String::from(k), String::from(v));
+                    deck.vars.insert(k.to_string(), v.trim().to_string());
                 } else {
                     return Err(format!("Deck var error: {} in {}", line, file));
                 }
@@ -76,13 +75,13 @@ pub fn load_deck_from_file(file: &str) -> Result<Deck, String> {
             _ => deck.content.push(line.into()),
         }
     }
-    
+
     let mut columns:u8 = 0;
     for s in &deck.content {
         let cols = s.split(':').collect::<Vec<&str>>().len() as u8;
         if cols > columns { columns = cols; }
     }
     deck.cols = columns;
-    
+
     Ok(deck)
 }
--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,25 @@ extern crate rand;
 mod flashcard;
 use std::io;
 use flashcard::*;
+use std::process;
 use rand::prelude::*;
 
 
 fn main() {
-    
-    if let Some(deck) = prompt_for_deck() {    
-        //debug stuff:
-        println!("Deck reads ok, cards: {}", deck.content.len());
-        print_deck(&deck);
-        println!("============================");
-        
-        println!("Session variables:");
-        for (k, v) in deck.vars.iter() {
-            println!("{}: {}", k, v);
-        }
+
+    let deck = prompt_for_deck().unwrap_or_else(|e| {
+        eprintln!("You fucked up!\n {}", e);
+        process::exit(1);
+    });
+
+    //debug stuff:
+    println!("Deck reads ok, cards: {}", deck.content.len());
+    print_deck(&deck);
+    println!("============================");
+
+    println!("Session variables:");
+    for (k, v) in deck.vars.iter() {
+        println!("{}: {}", k, v);
     }
 }
 
@@ -29,14 +33,15 @@ fn print_deck(deck: &Deck) {
     }
 }
 
-fn prompt_for_deck() -> Option<Deck> {
+fn prompt_for_deck() -> Result<Deck, &'static str> {
     println!("enter file name to load:");
-    
+
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("nigga what");
     let file = input.trim();
-    
-    Deck::new_from_file(&file)
+
+    match Deck::new_from_file(&file) {
+        Some(d) => Ok(d),
+        None => Err("Bad file or something nigga"),
+    }
 }
-
-
